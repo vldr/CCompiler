4e93eb9d05683223240b595ed4bfc125cf7b1b83
Vlad <vlader101@gmail.com>
2021-05-08 22:12:32 -0500

    Added unversioned files.

---
+++ source/Statements/StatementExpression.ts
@@ -0,0 +1,43 @@
+import Compiler from "../Compiler";
+import Statement from "./Statement";
+import Instruction from "../Instructions/Instruction";
+import Type from "../Types/Type";
+import TypeInteger from "../Types/TypeInteger";
+import Qualifier from "../Qualifiers/Qualifier";
+import QualifierConst from "../Qualifiers/QualifierConst";
+import TypeUnsignedInteger from "../Types/TypeUnsignedInteger";
+import TypeFloat from "../Types/TypeFloat";
+import ExternalErrors from "../Errors/ExternalErrors";
+import InternalErrors from "../Errors/InternalErrors";
+import QualifierNone from "../Qualifiers/QualifierNone";
+import DestinationVariable from "../Destinations/DestinationVariable";
+import TypeStruct from "../Types/TypeStruct";
+import Utils from "../Utils";
+import NodeDeclarator from "../Nodes/NodeDeclarator";
+import Variable from "../Variables/Variable";
+import VariablePrimitive from "../Variables/VariablePrimitive";
+import VariableStruct from "../Variables/VariableStruct";
+import NodeReturn from "../Nodes/NodeReturn";
+import TypeVoid from "../Types/TypeVoid";
+import DestinationNone from "../Destinations/DestinationNone";
+import DestinationStack from "../Destinations/DestinationStack";
+import InstructionRTN from "../Instructions/InstructionRTN";
+import ExpressionResultVariable from "../Expressions/ExpressionResultVariable";
+import ExpressionResultAccessor from "../Expressions/ExpressionResultAccessor";
+import NodeExpression from "../Nodes/NodeExpression";
+
+export default class StatementExpression extends Statement
+{
+    public generateAndEmit(): void
+    {
+        const node = this._node as NodeExpression;
+
+        const expressionResult = this._compiler.generateExpression(
+            new DestinationNone(new TypeVoid(new QualifierNone(), 1)),
+            this._scope,
+            node.expression
+        );
+
+        this._compiler.emitToFunctions(expressionResult.write());
+    }
+}
