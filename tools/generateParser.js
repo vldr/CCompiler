c34a8bf7762227adcd23a64584e3e441e498d801
Vlad <vlader101@gmail.com>
2021-05-14 13:42:32 -0500

    Created a new directory, tools, containing all our "tools" script files.

--- tools/generateParser.js
+++ tools/generateParser.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const peg = require("pegjs");
 
-const INPUT_FILE_NAME = "c15s.pegjs";
-const OUTPUT_FILE_NAME = "parser.umd.js";
+const INPUT_FILE_NAME = "../parser/c15s.pegjs";
+const OUTPUT_FILE_NAME = "../parser/parser.umd.js";
 
 const data = fs.readFileSync(INPUT_FILE_NAME, 'utf8')
 const parser = peg.generate(data, { output: "source" });
