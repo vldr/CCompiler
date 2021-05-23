1c7d52492948cab0df0b3a2e64fb3961af9ea898
vlad <vlad@vldr.org>
2021-05-23 01:31:21 -0500

    Added new compiler test.

--- tests/Compiler.test.ts
+++ tests/Compiler.test.ts
@@ -1383,3 +1383,71 @@ test("Test 'radix_sort.c'.", async () => {
             .toStrictEqual(new Uint32Array([ value ]));
     });
 });
+
+test("Test 'odd_even_sort.c'.", async () => {
+    const compiler = new Compiler();
+    const result = compiler.compile(`
+        int arr[] = {
+            55, 47, 35, 15, 20, 42,
+            52, 30, 58, 15, 13, 19,
+            32, 18, 44, 11, 7, 9,
+            34, 56, 17, 25, 14, 48,
+            40, 4, 5, 7, 36, 1,
+            33, 49, 25, 26, 30, 9
+        };
+        
+        void oddEvenSort()
+        {
+            int n = arr.length;
+            int isSorted = 0;
+        
+            while (!isSorted)
+            {
+                isSorted = 1;
+                int temp = 0;
+        
+                for (int i = 1; i <= n - 2; i = i + 2)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                        isSorted = 0;
+                    }
+                }
+        
+                for (int i = 0; i <= n - 2; i = i + 2)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                        isSorted = 0;
+                    }
+                }
+            }
+        }
+        
+        oddEvenSort();
+    `);
+
+    const interpreter = new Interpreter(result);
+    await interpreter.run();
+
+    const sortedList = [
+        55, 47, 35, 15, 20, 42,
+        52, 30, 58, 15, 13, 19,
+        32, 18, 44, 11, 7, 9,
+        34, 56, 17, 25, 14, 48,
+        40, 4, 5, 7, 36, 1,
+        33, 49, 25, 26, 30, 9
+    ].sort((a, b) => a - b);
+
+    sortedList.forEach((value, index) =>
+    {
+        expect(interpreter.memoryRegions.get(`var_arr_${index}`))
+            .toStrictEqual(new Uint32Array([ value ]));
+    });
+});
